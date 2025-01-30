e03b99175d4e9f9a0a9cac52abdb2864bb386998
Filip-Povidernyi <p.poviderniy@gmail.com>
2025-01-30 13:49:13 +0200

    fixed registr form

--- Screens/RegistrationScreen.js
+++ Screens/RegistrationScreen.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
-import { ImageBackground, Keyboard, KeyboardAvoidingView, Platform, StyleSheet, Text, TouchableWithoutFeedback, View } from "react-native";
+import { ImageBackground, Keyboard, KeyboardAvoidingView, Platform, Text, TouchableWithoutFeedback, View } from "react-native";
 import image from "../assets/images/PhotoBG.png"
 import AddIcon from "../icons/AddIcon";
 import InputField from "../components/InputField";
 import PasswordInput from "../components/PasswordInput";
-import Button from "../components/Button";
+import Button from "../components/Button"
+import styles from "../styles/registrationStyles.js"
 
 
 
@@ -18,25 +19,13 @@ const RegistrationScreen = () => {
     const [isPasswordFocus, setIsPasswordFocus] = useState(false);
     const [keyboardVisible, setKeyboardVisible] = useState(false);
 
-    const handleLoginChange = (value) => {
-        setLoginText(value);
-        setKeyboardVisible(true);
-    };
 
-    const handleEmailChange = (value) => {
-        setEmailText(value);
-        setKeyboardVisible(true);
-    };
-
-    const handlePasswordChange = (value) => {
-        setPassword(value);
-        setKeyboardVisible(true);
-    };
     const HandleSubmit = () => {
         console.log([loginText, emailText, password]);
         setLoginText("");
         setEmailText("");
         setPassword("");
+        console.log(keyboardVisible)
     };
 
     return (
@@ -51,7 +40,7 @@ const RegistrationScreen = () => {
                 >
                     <View style={{
                         ...styles.registrContainer,
-                        height: keyboardVisible ? 549 : 374,
+                        height: keyboardVisible ? 374 : 549,
                     }}>
                         <View style={styles.avatar}>
                             <AddIcon style={styles.iconAdd} />
@@ -62,26 +51,26 @@ const RegistrationScreen = () => {
                                 <InputField
                                     value={loginText}
                                     placeholder="Логін"
-                                    onFocus={() => setIsLoginFocus(true)}
-                                    onBlur={() => setIsLoginFocus(false)}
-                                    onTextChange={handleLoginChange}
+                                    onFocus={() => { setIsLoginFocus(true); setKeyboardVisible(true) }}
+                                    onBlur={() => { setIsLoginFocus(false); setKeyboardVisible(false) }}
+                                    onTextChange={(value) => setLoginText(value)}
                                     isFocus={isLoginFocus}
                                 />
                                 <InputField
                                     value={emailText}
                                     placeholder="Електронна пошта"
                                     keyboardType="email-address"
-                                    onFocus={() => setIsEmailFocus(true)}
-                                    onBlur={() => setIsEmailFocus(false)}
-                                    onTextChange={handleEmailChange}
+                                    onFocus={() => { setIsEmailFocus(true); setKeyboardVisible(true) }}
+                                    onBlur={() => { setIsEmailFocus(false); setKeyboardVisible(false) }}
+                                    onTextChange={(value) => setEmailText(value)}
                                     isFocus={isEmailFocus}
                                 />
                                 <PasswordInput
                                     value={password}
                                     placeholder="Пароль"
-                                    onFocus={() => setIsPasswordFocus(true)}
-                                    onBlur={() => setIsPasswordFocus(false)}
-                                    onTextChange={handlePasswordChange}
+                                    onFocus={() => { setIsPasswordFocus(true); setKeyboardVisible(true) }}
+                                    onBlur={() => { setIsPasswordFocus(false); setKeyboardVisible(false) }}
+                                    onTextChange={(value) => setPassword(value)}
                                     isFocus={isPasswordFocus}
                                     autoCapitalize={false}
                                 />
@@ -108,85 +97,4 @@ const RegistrationScreen = () => {
         </ImageBackground >
     )
 };
-
-const styles = StyleSheet.create({
-    container: {
-        marginRight: 16,
-        marginLeft: 16,
-    },
-    bgimage: {
-        flex: 1,
-        width: "100%",
-        height: "100%",
-        justifyContent: "flex-end",
-    },
-    registrContainer: {
-        width: "100%",
-        borderTopRightRadius: 25,
-        borderTopLeftRadius: 25,
-        paddingTop: 92,
-        backgroundColor: '#fff',
-        alignSelf: "center",
-
-    },
-    avatar: {
-        width: 120,
-        height: 120,
-        position: "relative",
-        marginTop: -159,
-        marginBottom: 32,
-        alignSelf: "center",
-        backgroundColor: "#f6f6f6",
-        borderRadius: 16,
-    },
-    iconAdd: {
-        position: "absolute",
-        bottom: 16,
-        right: -12,
-    },
-    title: {
-        fontFamily: "Roboto-Medium",
-        textAlign: "center",
-        color: "#212121",
-        fontSize: 30,
-        fontWeight: 500,
-        letterSpacing: 0.3,
-        marginBottom: 33,
-
-    },
-    inputsCont: {
-        gap: 16,
-        marginBottom: 43,
-    },
-    inputTextContainer: {
-        height: 50,
-        borderColor: "#E8E8E8",
-        borderRadius: 8,
-        borderWidth: 1,
-        backgroundColor: "#F6F6F6",
-        paddingHorizontal: 16,
-        fontFamily: "Roboto-Regular",
-        fontSize: 16,
-        fontWeight: 400,
-    },
-    onFocused: {
-        borderColor: "#FF6C00",
-    },
-    baseText: {
-        fontWeight: "400",
-        fontSize: 16,
-        lineHeight: 18,
-    },
-    buttonText: {
-        color: "#FFF",
-        textAlign: "center",
-    },
-    btnContainer: {
-        marginBottom: 16,
-    },
-    loginContainer: {
-        alignItems: "center",
-    },
-});
-
 export default RegistrationScreen;
